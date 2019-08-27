84d8c0b3a2746c278902f181430b5c8a3523f3c1
Justin Briggs <justinwbriggs@gmail.com>
2019-08-27 15:59:14 -0400

    Removed a few unused classes.

--- src/app/models/exercise.ts
+++
@@ -1,5 +0,0 @@
-export class Exercise {
-  complete: boolean = false;
-  goal: string;
-  constructor() {}
-}
--- src/app/models/lesson-category.ts
+++
@@ -1,9 +0,0 @@
-export class LessonCategory {
-
-    name: string;
-
-    constructor() {
-
-    }
-
-}
